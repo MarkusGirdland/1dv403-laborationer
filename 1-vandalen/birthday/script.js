ade5fa4b7b838c6af1a1eacc6f0d88ba571e528d
OpenShift guest <mg222pi@student.lnu.se>
2013-12-02 18:08:32 -0500

    Laboration 1.3 - födelsedagen - klart

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -2,15 +2,107 @@
 
 window.onload = function(){
 
+	var i = 0;
 	
 	var birthday = function(date){
+	
+	var character = " ";
+	var year = " ";
+	var month = " ";
+	var day = " ";
 		
+	try
+	{
+        for(i = 0; i < date.length; i+=1)       // For loop för hela datumets längd
+        {
+            character = date.charAt(i);
+            
+            if((i != 4 || i != 7) && (i >= 0 && i <= 9))        // Så länge i är mellan 0 och 9 men inte 4 eller 7
+            {
+                if(character < 0 || character > 9)
+                {
+                    throw new WrongDateException();
+                }
+                
+                else
+                {
+                    if(i <= 3)       // Om året matas in
+                    {
+                        saveYear();
+                    }
+                    
+                    if(i === 5 || i === 6)      // Om månades matas in
+                    {
+                        saveMonth();
+                    }
+                    
+                    if(i === 8 || i === 9)      // Om dagen matas in
+                    {
+                        saveDay();
+                    }
+                }
+            }
+            
+            if(i === 4 || i === 7) // Om det ska vara bindestreck
+            {
+                if(character != "-")
+                {
+                    throw new WrongDateException();
+                }
+            }
+        } 
+    
+        
+        var birthdayDate = new Date(year, month-1, day);
+        var now = new Date();
+        
+        var days = 0;
+        
+        console.log(birthdayDate);
+        console.log(now);
+        
+        days = ( (birthdayDate.getTime() - now.getTime())  / (1000*60*60*24));
+        
+        if(days < 0)            // Om man redan fyllt år
+        {
+            
+            var yearDiff = birthdayDate.getYear() - now.getYear();
+            
+            yearDiff *= -1;         // Gör om till positivt nummer      
+            yearDiff++;
+            days = days + (yearDiff  * 365);
+        }
+        
+        days = Math.round(days);
+        return days;
 
+      
+	}
+	
+	catch(err)
+	{
+        return "Fel format på datum. Försök igen.";    
+	}
 
-			// Din kod här.
+    function WrongDateException()
+    {
+        return "ett oglitigt antal";
+    }
+    
+    function saveYear()
+    {
+        year += character;
+    }
+    
+    function saveMonth()
+    {
+        month += character;
+    }
 
-
-
+    function saveDay()
+    {
+        day += character;
+    }
 
 	};
 	// ------------------------------------------------------------------------------
